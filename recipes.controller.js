69074641a15d031d8524c7423833181c854b38d8
Lucian Voinea <lucian.voinea@gmail.com>
2016-10-10 23:38:37 +0200

    Added gui interaction for selecting recipes froomn the recipe list view

--- recipes.controller.js
+++ recipes.controller.js
@@ -14,6 +14,8 @@
         vm.edit = edit;
         
         vm.loading = false;
+        vm.selecting = false;
+        
         vm.nameFilter = '';
         vm.categoryFilter = '';
         vm.clearNameFilter = clearNameFilter;
@@ -40,8 +42,32 @@
         }
         
         function selectRecipe(recipe){
-            //TODO
-            return DataService.updateShoppingListRecipe(recipe);
+            vm.selecting = true;
+            if (recipe.in_shopping_list){
+                DataService.addShoppingListRecipe(recipe.id)
+                .then(function(response){
+                    recipe.in_shopping_list = true;
+                })
+                .catch(function(error){
+                    AlertService.setAlert('ERROR: Could not add receipe to shoopping list  (code ' + error.status + ').');
+                })
+                .finally(function(){
+                    vm.selecting = false;
+                });
+            }
+            else if (!recipe.in_shopping_list){
+                vm.selecting = true;
+                DataService.removeShoppingListRecipe(recipe.id)
+                .then(function(response){
+                    recipe.in_shopping_list = false;
+                })
+                .catch(function(error){
+                    AlertService.setAlert('ERROR: Could not remove recipe from shoopping list  (code ' + error.status + ').');
+                })
+                .finally(function(){
+                    vm.selecting = false;
+                });
+            }
         }
         
         function clearNameFilter() {
